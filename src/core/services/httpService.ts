3f61f42941d48d8e2d663d5100730a2d66fb4569
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-18 22:49:43 +0330

    add: httpServices for manage request

---
+++ src/core/services/httpService.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const app = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true,
+});
+
+app.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+app.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+    if (err.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      try {
+        toast.error("لاگین کنید");
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
+
+const http = {
+  get: app.get,
+  post: app.post,
+  delete: app.delete,
+  put: app.put,
+  patch: app.patch,
+};
+
+export default http;
