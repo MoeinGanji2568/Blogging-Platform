5e3e2765f33db3d167a129e64b065f99d3279ee2
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-18 23:12:59 +0330

    update: update App because wrap react query provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import { publicRoutes } from "../config/router/public.router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
-  return <RouterProvider router={publicRoutes} />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={publicRoutes} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
