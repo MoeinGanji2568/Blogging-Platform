4f2f53e2ea373c59bb32ccd6062e384cb12851c6
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-18 23:07:09 +0330

    update: some change in postService

--- src/core/services/postService.ts
+++ src/core/services/postService.ts
@@ -1,5 +1,8 @@
 import http from "./httpService";
 
 export async function getPosts() {
-  return http.post(`/auth/sign-up`).then(({ data }) => data);
+  return http.post(`/post/list`).then(({ data }) => data);
 }
+
+const postApi = { getPosts };
+export default postApi;
