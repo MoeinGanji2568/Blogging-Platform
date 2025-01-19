c1862cf565516a96156a69a184fad6e43699ff89
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-19 21:11:27 +0330

    add: coverImage for img in blogCard

--- src/components/Blogs/CoverImage.tsx
+++ src/components/Blogs/CoverImage.tsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
+import { PostTypes } from "../../types/blogPost/post.type";
 
-interface Props {
-  slug: string;
-  coverImageUrl: string;
-}
-export default function CoverImage({ slug, coverImageUrl }: Props) {
+const CoverImage: React.FC<Pick<PostTypes, "slug" | "coverImageUrl">> = ({
+  slug,
+  coverImageUrl,
+}) => {
   return (
     <div className="relative aspect-video overflow-hidden rounded-lg mb-6">
       <Link to={`/blogs/${slug}`}>
         <img
-          className="object-cover object-center hover:scale-110 transition-all ease-out duration-300"
+          className="object-cover object-center hover:scale-110 transition-all ease-out duration-300 "
           src={coverImageUrl}
         />
       </Link>
     </div>
   );
-}
+};
+export default CoverImage;
