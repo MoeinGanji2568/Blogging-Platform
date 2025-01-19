37fcfe23bdae56f2d015b6fbb58a5c9b702b210e
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-19 21:08:41 +0330

    add: author component for show who wright the blog in blogCard

--- src/components/Blogs/Author.tsx
+++ src/components/Blogs/Author.tsx
@@ -1,11 +1,15 @@
+import { AuthorType } from "../../types/blogPost/post.type";
 import Avatar from "../ui/Avatar";
 
-function Author({ avatarUrl, name }: { avatarUrl: string; name: string }) {
+const Author: React.FC<Pick<AuthorType, "avatarUrl" | "name">> = ({
+  avatarUrl,
+  name,
+}) => {
   return (
     <div className="flex items-center gap-x-1">
       <Avatar src={avatarUrl} />
       <span className="text-sm text-secondary-500">{name}</span>
     </div>
   );
-}
+};
 export default Author;
