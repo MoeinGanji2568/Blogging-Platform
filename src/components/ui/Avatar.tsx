0c48ff5886c1aa6179e85941993fadf76e092dde
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-19 21:08:06 +0330

    add:avatar component

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,19 +1,20 @@
+import manPicture from "../../../public/man-pic.webp";
 function Avatar({
   src,
   width = 24,
   height = 24,
 }: {
-  src: string;
+  src: string | null;
   width?: number;
   height?: number;
 }) {
   return (
     <img
-      src={src || "/images/avatar.png"}
+      src={src || manPicture}
       width={width}
       height={height}
       className="rounded-full ring-1 ring-secondary-300 ml-2"
-      alt={src}
+      alt={src || "/images/avatar.png"}
     />
   );
 }
