98cf9246d7e286a95374b32bba1de48117948198
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-20 15:08:30 +0330

    update: add get post by id in post service

--- src/core/services/postService.ts
+++ src/core/services/postService.ts
@@ -3,6 +3,9 @@ import http from "./httpService";
 export async function getPosts() {
   return http.get(`/post/list`).then(({ data }) => data);
 }
+export async function getPostById(id: string) {
+  return http.get(`/post/${id}`).then(({ data }) => data);
+}
 
-const postApi = { getPosts };
+const postApi = { getPosts, getPostById };
 export default postApi;
