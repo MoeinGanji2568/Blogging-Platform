b94046d7ef40027c909b54049a961e023d64d2e2
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-20 15:09:24 +0330

    update: change link value to blogId

--- src/components/Blogs/CoverImage.tsx
+++ src/components/Blogs/CoverImage.tsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 import { PostTypes } from "../../types/blogPost/post.type";
+import notFoundImg from "../../../public/notFoundImg.webp";
 
-const CoverImage: React.FC<Pick<PostTypes, "slug" | "coverImageUrl">> = ({
-  slug,
+const CoverImage: React.FC<Pick<PostTypes, "_id" | "coverImageUrl">> = ({
+  _id,
   coverImageUrl,
 }) => {
   return (
     <div className="relative aspect-video overflow-hidden rounded-lg mb-6">
-      <Link to={`/blogs/${slug}`}>
+      <Link to={`/blogs/${_id}`}>
         <img
           className="object-cover object-center hover:scale-110 transition-all ease-out duration-300 "
-          src={coverImageUrl}
+          src={coverImageUrl || notFoundImg}
         />
       </Link>
     </div>
