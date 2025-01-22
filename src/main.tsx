15bcf6973c19db8d00393dba895ef7c8ead744b9
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-22 21:57:08 +0330

    update: wrap auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 import "./assets/css/fonts.css";
+import AuthProvider from "./contexts/auth/auth.provider.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
