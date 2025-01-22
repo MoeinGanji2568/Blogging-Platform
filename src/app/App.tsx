9ccb227c6ae2234e1851bed3ef30fa68c48d9cc9
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-22 21:59:16 +0330

    update: check isAuthenticated for  access routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,23 @@ import { RouterProvider } from "react-router-dom";
 import { publicRoutes } from "../config/router/public.router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "../contexts/auth/auth.provider";
+import { authRoutes } from "../config/router/auth.router";
+import { Spinner } from "../components/ui/Spinner";
 
 function App() {
   const queryClient = new QueryClient();
+  const { isAuthenticated } = useAuth();
+  console.log(isAuthenticated);
+
+  if (isAuthenticated === null) return <Spinner />;
+
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={publicRoutes} />
+      <RouterProvider router={isAuthenticated ? authRoutes : publicRoutes} />
       <ReactQueryDevtools />
+      <Toaster />
     </QueryClientProvider>
   );
 }
