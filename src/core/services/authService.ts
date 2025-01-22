20f4674185792862a96dcd0e275e20483abd6799
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-22 21:55:12 +0330

    add: add auth service

---
+++ src/core/services/authService.ts
@@ -0,0 +1,16 @@
+import http from "./httpService";
+
+export async function signUpApi(data: {
+  email: string;
+  password: string;
+  name: string;
+}) {
+  return http.post(`/user/signup`, data).then(({ data }) => data);
+}
+export async function signInApi(data: { email: string; password: string }) {
+  return http.post(`/user/signin`, data).then(({ data }) => data);
+}
+
+export async function verifyApi() {
+  return http.get(`/user/profile`).then(({ data }) => data);
+}
