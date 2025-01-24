57a1a61759c669df37d99061a3f3bb4aa3e09d45
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-01-24 23:04:22 +0330

    update: add handle like post function

--- src/components/Blogs/BlogInteraction.tsx
+++ src/components/Blogs/BlogInteraction.tsx
@@ -10,20 +10,37 @@ import {
 import { toPersianDigits } from "../../utils/toPersianNumber";
 import ButtonIcon from "../ui/ButtonIcon";
 import { PostTypes } from "../../types/blogPost/post.type";
+import { useLikePost } from "../../hooks/useLikePost";
+import { useAuth } from "../../contexts/auth/auth.provider";
+import toast from "react-hot-toast";
 
 const BlogInteraction: React.FC<Partial<PostTypes>> = ({
   commentsCount,
   likesCount,
   isLiked,
   isBookmarked,
+  _id,
 }) => {
+  const { isAuthenticated } = useAuth();
+  const likePost = useLikePost();
+
+  const handleLikePost = (postId: string) => {
+    if (isAuthenticated)
+      try {
+        likePost.mutate(postId);
+      } catch (error) {
+        console.log(error);
+      }
+    else toast.error("لطفا ابتدا ورود کنید ...");
+  };
+
   return (
     <div className="flex items-center gap-x-4">
       <ButtonIcon variant="secondary">
         <ChatBubbleOvalLeftEllipsisIcon />
         <span>{toPersianDigits(commentsCount || 0)}</span>
       </ButtonIcon>
-      <ButtonIcon variant="red">
+      <ButtonIcon variant="red" onClick={() => handleLikePost(_id!)}>
         {isLiked ? <SolidHearIcon /> : <HeartIcon />}
         <span>{toPersianDigits(likesCount || 0)}</span>
       </ButtonIcon>
