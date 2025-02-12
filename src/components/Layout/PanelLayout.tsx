272f5d83619e9971ac78b8975350d02eb1f13a12
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-02-12 16:52:24 +0330

    update: add header and sideBar component in panel layout

--- src/components/Layout/PanelLayout.tsx
+++ src/components/Layout/PanelLayout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
+import PanelHeader from "../Panel/Panelheader";
+import PanelSideBar from "../Panel/PanelSideBar";
 
 const PanelLayout = () => {
   return (
     <div className="bg-secondary-0">
       <div className="grid grid-cols-12 h-screen">
         <aside className="col-span-12 lg:col-span-3 xl:col-span-2 hidden lg:block">
-          sideBar
+          <PanelSideBar />
         </aside>
         <div className="col-span-12 lg:col-span-9 xl:col-span-10 h-screen flex flex-col">
-          header
+          <PanelHeader />
           <main className="bg-secondary-100 rounded-tr-3xl p-4 md:p-6 lg:p-10 flex-1 overflow-y-auto">
             <div className="xl:max-w-screen-xl">
               <Outlet />
